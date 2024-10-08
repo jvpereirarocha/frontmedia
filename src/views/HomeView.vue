78827fcce3be8e82ce48ec7eea3fb84ab74f2b19
Joao Victor <jvpereirarocha@gmail.com>
2024-10-08 12:18:59 -0300

    feat: adding views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+const items = [
+  { name: 'AWS', url: 'https://www.youtube.com/embed/mkHMYCnHl0U?si=ALDGxQ15X2KiztN7' },
+  { name: 'Expresso VDG', url: 'https://www.youtube.com/embed/qh51mcuH7Fg' },
+  { name: 'AWS', url: 'https://www.youtube.com/embed/mkHMYCnHl0U?si=ALDGxQ15X2KiztN7' },
+  { name: 'Expresso VDG', url: 'https://www.youtube.com/embed/qh51mcuH7Fg' }
+]
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1 class="title">Media list</h1>
+    <ul class="list-items cards">
+      <li v-for="(item, index) in items" :key="index">
+        <div class="card">
+          <div class="card-header">
+            <h2 class="subtitle">{{ item.name }}</h2>
+          </div>
+          <div class="card-body">
+            <iframe class="card-img" width="400" height="300" :src="item.url"></iframe>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
---
+++ src/views/UploadView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1 class="title">Salvar vídeo</h1>
+    <form method="POST">
+      <div class="form-container">
+        <div class="form-group">
+          <label for="title"> Título do vídeo</label>
+          <input type="text" class="form-control" placeholder="Insira um título para o vídeo" />
+        </div>
+        <div class="form-group">
+          <label for="title">URL do vídeo no Youtube</label>
+          <input
+            type="text"
+            class="form-control"
+            aria-describedby="videoUrl"
+            placeholder="Cole a URL do vídeo copiada do Youtube"
+          />
+          <small id="videoUrl" class="text-muted">
+            Vá até o vídeo desejado no Youtube, clique com o botão direito e selecione "Copiar URL
+            do vídeo"</small
+          >
+        </div>
+        <div class="button-container">
+          <input type="submit" class="button" value="Salvar" />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
